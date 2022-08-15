1f1305c673d34ea838b3ed470a52adbe6c8b5581
Matheus Rodrigues <62529344+contrastguy@users.noreply.github.com>
2022-08-15 00:19:27 -0300

    Adding the CRUD application for Login

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,9 +1,45 @@
-from flask import Flask
+
+from flask import Flask , render_template, request ,url_for
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__,template_folder="template")
+
+load_dotenv("flaskr\.env")
+url= os.environ["MONGODB_URI"]
+client = MongoClient(url)
+
+db = client.Desafio
+loginCollection= db.login
+cadastroCollection = db.cadastro
 
 
-app = Flask(__name__)
+class User:
+
+    def verifyuser(self):
+            username = request.form.get("username")
+            password = request.form.get("password")
+            loginCollection.insert_many({username,password})
+            if loginCollection.find({username,password}) == False:
+                cadastroCollection.insert_one({username,password})
+        
 
 
-@app.route("/")
-def test(): 
-    return()
+@app.route("/",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+       return User.verifyuser()
+    
+    return render_template("index.html")
+
+    
+
+
+
+
+
+
+
+
