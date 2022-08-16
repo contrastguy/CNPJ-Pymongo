7cb44e5deb1efdbd410b7114b522e54ebdff0343
Matheus Rodrigues <62529344+contrastguy@users.noreply.github.com>
2022-08-15 22:51:51 -0300

    Adding the  completed CRUD

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,11 +1,12 @@
-
-from flask import Flask , render_template, request ,url_for
+from flask import Flask , render_template, request ,redirect,url_for
 import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+from os import error
 
 app = Flask(__name__,template_folder="template")
+
 
 load_dotenv("flaskr\.env")
 url= os.environ["MONGODB_URI"]
@@ -14,32 +15,72 @@ client = MongoClient(url)
 db = client.Desafio
 loginCollection= db.login
 cadastroCollection = db.cadastro
+collectionEmpresas = db.Empresas
+collectionSocios = db.Sócios
+collectionsEstabelecimentos = db.Estabelecimentos
 
-
-class User:
-
-    def verifyuser(self):
-            username = request.form.get("username")
-            password = request.form.get("password")
-            loginCollection.insert_many({username,password})
-            if loginCollection.find({username,password}) == False:
-                cadastroCollection.insert_one({username,password})
-        
+error = None
 
 
 @app.route("/",methods=["GET","POST"])
-def login():
+def register():
+    
+    if request.method == "GET":
+        return render_template("index.html")
+    
     if request.method == "POST":
-       return User.verifyuser()
+       username = request.form["username"]
+       password = request.form["password"]
+       error =  None
+       
+       if not username:
+           error = "É preciso do Username"
+       elif not password:
+           error  = "É preciso da Password" 
+       
+       if error is None:
+           try:
+               cadastroCollection.insert_one({"Username":username,"Password":password})
+           except cadastroCollection.find({"Username":username,"Password":password}):
+               error=f"Usuário {username} já cadastrado"  
+               loginCollection.insert_one({"Username":username,"Password":password}) 
+               
+    return redirect('/data') 
+    #    if loginCollection.find({username,password}) == False:
+    #        print("Você ainda não está cadastrado")
+    #        cadastroCollection.insert_one({"username":username,"password":password})
+    #    else:
+    #        loginCollection.insert_one({"username":username,"password":password})
     
-    return render_template("index.html")
-
     
 
+ 
+@app.route('/data',methods=["GET","POST"])
+def datalist():  
+    if request.method == "GET":
+        return redirect('/filter')
+    
+    if request.method == "POST":
+         return render_template("datalist.html")
 
 
 
-
-
-
-
+@app.route('/filter',methods=["GET","POST"])
+def filter():
+    if request.method == "GET":
+         cnpjem =  request.form["cnpjem"]
+         rses = request.form["rses"]
+         lso  = request.form["lso"]
+         if cnpjem and rses and lso == True:
+          a =  collectionEmpresas.find_one({cnpjem})
+          b =  collectionEmpresas.find_one({rses})
+          c =  collectionsEstabelecimentos.find_one({lso})
+          if a and b and c == True:
+                   return "<p>Os dados são verdadeiros</p>"
+          else:     
+                    return "<p>Os dados não são verdadeiros</p>"
+    
+    
+    if request.method == "POST": 
+         return render_template('filter.html',cnpjem=cnpjem,rses=rses,lso=lso)            
+    
